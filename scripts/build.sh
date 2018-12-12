242afb487c393c408bdb983ccf5bbf72322b0458
双磊(double) <sl3@meitu.com>
2018-12-12 18:08:28 +0800

    测试

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
-npm install -g yarn --registry=https://registry.npm.taobao.org
-yarn install
-yarn run build
+npm install --registry=https://registry.npm.taobao.org --max-old-space-size=100
+npm run build --max-old-space-size=100
